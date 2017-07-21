b210f44e96011b1f01ee35dc0d7ce8c8a998d7c7
Marrah <30308273+Kida021@users.noreply.github.com>
2017-07-21 10:01:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "1ce3e0ba-d900-41d8-83ec-0279c141e521",
-    appPassword: "MPf1F37ouqdAO2A34Ub7kpp"
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
